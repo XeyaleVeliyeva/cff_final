36f97f1cb6a74349c693bd54d59a2d01b72acb52
XeyaleVeliyeva <veliyevaxeyael12@gmail.com>
2025-06-03 12:50:02 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/cff-final">
     <App />
   </BrowserRouter>
 );
